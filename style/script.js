e0b177aaac8f922e6d8509763d4f7dc7c98e918e
TomDuss <tom.dussaussois@outlook.fr>
2022-12-11 22:54:01 +0100

    firstCommit

---
+++ style/script.js
@@ -0,0 +1,26 @@
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  loop: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+        center: true,
+        items: 1,
+        nav: false,
+        autoplay: false,
+    },
+    600: {
+        center: false,
+        items: 3,
+        nav: false,
+        autoplay: false,
+    },
+    1000: {
+        center: false,
+        items: 4,
+        nav: false,
+        autoplay: false,
+    },
+  },
+});
